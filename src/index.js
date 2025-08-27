0f1d6845c77792d8d9552037286dd06e2c62e6e2
zahidghotia786 <zahidghotia@gmail.com>
2025-08-27 02:32:40 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import dotenv from "dotenv";
 import prisma from "./config/db.js"; // ✅ DB connect hote hi check hoga
 import authRoutes from "./modules/auth/auth.routes.js";
 import adminRoutes from "./modules/admin/admin.routes.js";
-import emailRoutes from "./modules/emails/email.routes.js";
+import emailRoutes from "./modules/emails/email.routes.js"
+
+// import userRoutes from "./modules/users/users.routes.js";
+// import bookingRoutes from "./modules/bookings/bookings.routes.js";
+// import ticketRoutes from "./modules/tickets/tickets.routes.js";
+// import productRoutes from "./modules/products/products.routes.js";
+// import paymentRoutes from "./modules/payments/payments.routes.js";
+// import b2bRoutes from "./modules/b2b/b2b.routes.js";
+
 
 import { errorHandler } from "./middleware/errorHandler.js";
 
@@ -43,6 +51,13 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/email", emailRoutes);
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/bookings", bookingRoutes);
+// app.use("/api/tickets", ticketRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/payments", paymentRoutes);
+// app.use("/api/b2b", b2bRoutes);
 
 // Health Check
 app.get("/", (req, res) => {
